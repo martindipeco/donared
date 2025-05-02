064e317d98f5aa3381eb92ef36f70141d49d104a
martindipeco <martindipeco@gmail.com>
2025-05-02 12:15:01 -0300

    empezando con views

--- donaredapp/urls.py
+++ donaredapp/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = "donaredapp"
+
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:item_id>/", views.tarjeta, name="tarjeta"),
+    path("<int:item_id>/contacto/", views.contacto, name="contacto"),
+    path("<int:item_id>/pedir/", views.pedir, name="pedir"),
 ]
--- donaredapp/views.py
+++ donaredapp/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 from django.http import HttpResponse
+from .models import Item
 
 
 def index(request):
-    return HttpResponse("Hola, Mundo! - Bienvenidos a DonaRed!")
+    items_recientes = Item.objects.order_by("-fecha_creacion")[:5]
+    context = {"items_recientes": items_recientes}
+    return render(request, "donaredapp/index.html", context)
 
+def tarjeta(request, item_id):
+    item = get_object_or_404(Item, pk=item_id)
+    return render(request, "donaredapp/tarjeta.html", {"item": item})
 
+def contacto(request, item_id):
+    return HttpResponse("Estos son los datos de contacto del donante del item %s." % item_id)
+
+def pedir(request, item_id):
+    return HttpResponse("Estás solicitando el item %s." % item_id)
