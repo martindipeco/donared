73862cb34de851b35c52046e810ce8d90e50fc20
zangano <franco.rotella@hotmail.com>
2025-06-27 17:51:06 +0200

    actualizo la cantidad de caracteres disponibles para crear una noticia, antes era 500 ahora 5000

--- novedades/models.py
+++ novedades/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.core.exceptions import ValidationError
 
 class Noticia(models.Model):
     ESTADO_CHOICES = [
@@ -9,7 +10,7 @@ class Noticia(models.Model):
     ]
 
     titulo = models.CharField(max_length=200)
-    descripcion = models.TextField(max_length=500)
+    descripcion = models.TextField()
     imagen = models.ImageField(upload_to='noticias/')
     fecha_publicacion = models.DateTimeField(default=timezone.now)
     autor = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -24,3 +25,10 @@ class Noticia(models.Model):
 
     def __str__(self):
         return self.titulo
+
+    def clean(self):
+        super().clean()
+        if self.descripcion:
+            num_palabras = len(self.descripcion.split())
+            if num_palabras > 5000:
+                raise ValidationError({'descripcion': f'La descripción no puede tener más de 5000 palabras (actual: {num_palabras}).'})
